873ca0e390c8cf77cb29e0306e5d4a91a87e5dca
Ivan <154084167+hrytsev@users.noreply.github.com>
2026-07-16 16:16:43 +0300

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.py
@@ -0,0 +1,19 @@
+import random
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.quicksort(nums)
+    def quicksort(self,arr):
+        if len(arr)==1:
+            return [arr[0]]
+        if len(arr)==0:
+            return []
+        if len(arr)==2:
+            if arr[0]>arr[1]:
+                return [arr[1],arr[0]]
+            else:
+                return arr
+        pivot=int(random.random()*len(arr))
+        left=[n for n in arr if n<arr[pivot]]
+        right=[n for n in arr if n>arr[pivot]]
+        mid=[n for n in arr if n==arr[pivot]]
+        return self.quicksort(left) + mid + self.quicksort(right)
