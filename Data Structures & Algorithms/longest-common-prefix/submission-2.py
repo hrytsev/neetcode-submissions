33520557582f5b5781b0d18affe462c002dacd40
Ivan <154084167+hrytsev@users.noreply.github.com>
2026-08-04 11:28:25 +0300

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        prefix=strs[0]
+        for word in strs:
+            while  not word.startswith(prefix):
+                prefix=prefix[:-1]
+        return prefix
